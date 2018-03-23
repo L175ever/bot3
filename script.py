b4cd5359409e9abf4aaea4f22ba32d196bf694f2
L175ever <37677413+L175ever@users.noreply.github.com>
2018-03-23 12:09:14 +0300

    Update script.py
    2

--- script.py
+++ script.py
@@ -44,7 +44,7 @@ now = datetime.datetime.now()
 def main():  
     new_offset = None
     today = now.day
-    hour = now.hour
+    hour = now.hour + 6
 
     while True:
         greet_bot.get_updates(new_offset)
