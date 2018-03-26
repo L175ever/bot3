b601bf6adffd052988915fbcf2fa9bec49d7c4a7
L175ever <37677413+L175ever@users.noreply.github.com>
2018-03-26 13:34:41 +0300

    Update script.py

--- script.py
+++ script.py
@@ -75,7 +75,9 @@ def main():
         requestpost = requests.post(url2, data={'key': key, 'text': last_chat_text, 'lang': lang})
         response_data = requestpost.json()
         text = last_chat_text
+        tr = response_data.remove([','])
         print (type(response_data["text"]))
+        
         
         if last_chat_text.lower() in greetings and now.day == today and 6 <= hour < 12:
             greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
